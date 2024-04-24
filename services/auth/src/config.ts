657b6bce7653a9004f53a6b9ae678873a1f77a5b
Naim Islam <naimislam3545@gmail.com>
2024-04-25 00:38:24 +0600

    feat: user and user profile create controller added

---
+++ services/auth/src/config.ts
@@ -0,0 +1,3 @@
+export const authConfig = {
+	user_service: process.env.USER_SERVICE_URL,
+};
---
+++ services/auth/src/controllers/userRegistration.ts
@@ -0,0 +1,68 @@
+import { authConfig } from "@/config";
+import prisma from "@/prisma";
+import { UserCreateSchema } from "@/schemas";
+import axios from "axios";
+import bcrypt from "bcryptjs";
+import { NextFunction, Request, Response } from "express";
+
+const userRegistration = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		// Validate the request body
+		const parseBody = UserCreateSchema.safeParse(req.body);
+		if (!parseBody.success) {
+			return res.status(400).json({ errors: parseBody.error.errors });
+		}
+
+		// Check if the user already exists
+		const existingUser = await prisma.user.findUnique({
+			where: {
+				email: parseBody.data.email,
+			},
+		});
+
+		if (existingUser) {
+			return res.status(400).json({
+				message: "User already exists",
+			});
+		}
+
+		// Hash the password
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(parseBody.data.password, salt);
+
+		// Create the auth user
+		const user = await prisma.user.create({
+			data: {
+				...parseBody.data,
+				password: hashedPassword,
+			},
+			select: {
+				id: true,
+				name: true,
+				email: true,
+				role: true,
+				status: true,
+				verified: true,
+				createdAt: true,
+				updatedAt: true,
+			},
+		});
+
+		// Create the user profile by calling the user service
+		await axios.post(`${authConfig.user_service}/users`, {
+			authUserId: user.id,
+			name: user.name,
+			email: user.email,
+		});
+
+		return res.status(201).json(user);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export default userRegistration;
